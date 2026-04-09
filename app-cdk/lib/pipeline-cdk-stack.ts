d098ee1bf21e4f09c398509422796d167995477e
Workshop Participant <participant@example.com>
2026-04-09 19:35:01 +0000

    Move app-cdk into monorepo

---
+++ app-cdk/lib/pipeline-cdk-stack.ts
@@ -0,0 +1,128 @@
+import { Stack, StackProps, CfnOutput, aws_codeconnections as codeconnections } from 'aws-cdk-lib'
+import { Construct } from 'constructs'
+import * as codepipeline from 'aws-cdk-lib/aws-codepipeline'
+import * as codebuild from 'aws-cdk-lib/aws-codebuild'
+import * as codepipeline_actions from 'aws-cdk-lib/aws-codepipeline-actions'
+import * as ecr from 'aws-cdk-lib/aws-ecr'
+import * as iam from 'aws-cdk-lib/aws-iam'
+
+interface ConsumerProps extends StackProps {
+  ecrRepository: ecr.Repository
+}
+
+export class PipelineCdkStack extends Stack {
+  constructor(scope: Construct, id: string, props: ConsumerProps) {
+    super(scope, id, props)
+    const SourceConnection = new codeconnections.CfnConnection(
+      this,
+      'CICD_Workshop_Connection',
+      {
+        connectionName: 'CICD_Workshop_Connection',
+        providerType: 'GitHub',
+      },
+    )
+
+    const pipeline = new codepipeline.Pipeline(this, 'Pipeline', {
+      pipelineName: 'CICD_Pipeline',
+      crossAccountKeys: false,
+      pipelineType: codepipeline.PipelineType.V2,
+      executionMode: codepipeline.ExecutionMode.QUEUED,
+    })
+
+    const codeBuild = new codebuild.PipelineProject(this, 'CodeBuild', {
+      environment: {
+        buildImage: codebuild.LinuxLambdaBuildImage.AMAZON_LINUX_2023_NODE_22,
+        computeType: codebuild.ComputeType.LAMBDA_10GB,
+      },
+      buildSpec: codebuild.BuildSpec.fromSourceFilename('buildspec_test.yml'),
+    })
+
+    const organizationName = this.node.tryGetContext('organizationName')
+    const repoName = this.node.tryGetContext('repoName')
+    const sourceOutput = new codepipeline.Artifact()
+    const unitTestOutput = new codepipeline.Artifact()
+
+    pipeline.addStage({
+      stageName: 'Source',
+      actions: [
+        new codepipeline_actions.CodeStarConnectionsSourceAction({
+          actionName: 'GitHub',
+          owner: organizationName,
+          repo: repoName,
+          output: sourceOutput,
+          branch: 'main',
+          connectionArn: SourceConnection.attrConnectionArn,
+        }),
+      ],
+    })
+
+    pipeline.addStage({
+      stageName: 'Code-Quality-Testing',
+      actions: [
+        new codepipeline_actions.CodeBuildAction({
+          actionName: 'Unit-Test',
+          project: codeBuild,
+          input: sourceOutput,
+          outputs: [unitTestOutput],
+        }),
+      ],
+    })
+
+    const dockerBuild = new codebuild.PipelineProject(this, 'DockerBuild', {
+      environmentVariables: {
+        IMAGE_TAG: { value: 'latest' },
+        IMAGE_REPO_URI: { value: props.ecrRepository.repositoryUri },
+        AWS_DEFAULT_REGION: { value: process.env.CDK_DEFAULT_REGION },
+      },
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_7_0,
+        computeType: codebuild.ComputeType.LARGE,
+        privileged: true,
+      },
+      buildSpec: codebuild.BuildSpec.fromSourceFilename('buildspec_docker.yml'),
+    })
+
+    const dockerBuildRolePolicy = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      resources: ['*'],
+      actions: [
+        'ecr:GetAuthorizationToken',
+        'ecr:BatchCheckLayerAvailability',
+        'ecr:GetDownloadUrlForLayer',
+        'ecr:GetRepositoryPolicy',
+        'ecr:DescribeRepositories',
+        'ecr:ListImages',
+        'ecr:DescribeImages',
+        'ecr:BatchGetImage',
+        'ecr:InitiateLayerUpload',
+        'ecr:UploadLayerPart',
+        'ecr:CompleteLayerUpload',
+        'ecr:PutImage',
+      ],
+    })
+
+    dockerBuild.addToRolePolicy(dockerBuildRolePolicy)
+
+    const dockerBuildOutput = new codepipeline.Artifact()
+
+    pipeline.addStage({
+      stageName: 'Docker-Push-ECR',
+      actions: [
+        new codepipeline_actions.CodeBuildAction({
+          actionName: 'Docker-Build',
+          project: dockerBuild,
+          input: sourceOutput,
+          outputs: [dockerBuildOutput],
+        }),
+      ],
+    })
+
+    new CfnOutput(this, 'SourceConnectionArn', {
+      value: SourceConnection.attrConnectionArn,
+    })
+
+    new CfnOutput(this, 'SourceConnectionStatus', {
+      value: SourceConnection.attrConnectionStatus,
+    })
+  }
+}
